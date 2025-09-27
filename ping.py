a1ff9049c8f3c9765c2311f95c1431de29a0787a
lalith0407 <58086270+lalith0407@users.noreply.github.com>
2025-09-27 12:36:19 -0700

    Add headers to POST request in ping.py

--- ping.py
+++ ping.py
@@ -5,6 +5,10 @@ data = {
     "calendar_id": "efficient.ai.agency@gmail.com",
     "redis_url": "redis://default:675d88cdab9e49858ccd124a7e643914@fly-efficient-ai-1.upstash.io:6379"
 }
-response = requests.post(URL, json=data)
+headers = {
+    "Content-Type": "application/json",
+    "x-api-key": "https://cron-jobs-for-everyone.fly.dev/popuate-redis"  # must match what the endpoint is checking!
+}
+response = requests.post(URL, json=data, headers=headers)
 print("Status:", response.status_code)
 print("Response:", response.text)
